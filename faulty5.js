f8375e7e1eda5f523a9de6e117085f2ff756c05d
Radoslaw Czarnecki <radoslaw.czarnecki@wundermanthompson.com>
2024-08-08 11:49:15 +0200

    commit

--- faulty5.js
+++ faulty5.js
@@ -1,10 +1,5 @@
-const crypto = require('crypto');
-
-function hashPassword(password) {
-  // BAD: Using MD5 hash algorithm which is considered insecure
-  const hash = crypto.createHash('md5');
-  hash.update(password);
-  return hash.digest('hex');
-}
-
-hashPassword('mySecurePassword');
+var dom,
+    contactDetails,
+    phonesTemplate,
+    emailsTemplate,
+    dom;
