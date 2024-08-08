c3200907fb42249eb55602201fd5a64f145c9fe9
Radoslaw Czarnecki <radoslaw.czarnecki@wundermanthompson.com>
2024-08-08 11:23:33 +0200

    second commit

--- commandInjection.js
+++
@@ -1,4 +0,0 @@
-let child_process = require('child_process');
-
-let path = req.query.path;
-child_process.exec('ls ' + path);
--- reflectedXSS.js
+++
@@ -1,7 +0,0 @@
-let express = require('express');
-let app = express();
-
-app.get('/some/path', function(req, res) {
-  let username = req.query.username;
-  res.send('Hello ' + username);
-});
